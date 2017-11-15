0440ccd3a446389faf6fb6c22d97e1aa4d13ebd8
hengl <henglin.h@gmail.com>
2017-11-15 19:32:02 +0100

    sdqa model completed, to be tested.

--- code/reproduction/lib/models/SDQA/SDQA.py
+++ code/reproduction/lib/models/SDQA/SDQA.py
@@ -4,10 +4,13 @@ import tensorflow as tf
 class SDQA:
 
     def __init__(self, activation_fn, is_training, input_shape=48536,
-                 num_filter=32, num_hidden_fc=128, dropout=0.5, kernel_size=3):
+                 num_filter=32, num_hidden_fc=128, dropout=0.5, kernel_size=3,
+                 learning_rate=0.01, batch_size=128):
 
         self.activation_fn = activation_fn
         self.is_training = is_training
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
 
         self.num_filter = num_filter
         self.num_hidden_fc = num_hidden_fc
@@ -15,23 +18,51 @@ class SDQA:
         self.kernel_size = kernel_size
         self.input_shape = input_shape
 
-    def define_network(self, features):
+        self.input1 = tf.placeholder(tf.float32, shape=[None, input_shape])
+        self.input2 = tf.placeholder(tf.float32, shape=[None, input_shape])
 
-        with tf.variable_scope(None, default_name="ConvNet", reuse=True) as scope:
-            input_layer = tf.reshape(features, [-1, self.input_shape])
+        with tf.variable_scope("SiameseNet") as scope:
+            self.logit1 = self._define_network(self.input1)
+            scope.reuse_variables()
+            self.logit2 = self._define_network(self.input2)
 
-            conv1 = tf.layers.conv1d(
-                inputs=input_layer,
-                filters=self.num_filter,
-                kernel_size=self.kernel_size
-            )
+    def _define_network(self, features):
 
-            pool1 = tf.layers.max_pooling1d(inputs=conv1, pool_size=2, strides=2)
+        conv1 = tf.layers.conv1d(
+            inputs=features,
+            filters=self.num_filter,
+            kernel_size=self.kernel_size,
+            activation=self.activation_fn
+        )
 
-            relu = tf.nn.relu(pool1)
+        pool1 = tf.layers.max_pooling1d(inputs=conv1, pool_size=2, strides=2)
 
-            dropout = tf.layers.dropout(relu, rate=self.dropout, training=self.is_training)
+        dropout = tf.layers.dropout(pool1, rate=self.dropout, training=self.is_training)
 
-            logits = tf.layers.dense(inputs=dropout, units=self.num_hidden_fc)
+        logits = tf.layers.dense(inputs=dropout, units=self.num_hidden_fc)
 
-            return logits
+        return logits
+
+    def inference(self):
+        return tf.losses.cosine_distance(self.logit1, self.logit2)
+
+    def loss(self, label, margin=1):
+
+        cosine_dist = tf.losses.cosine_distance(self.logit1, self.logit2, reduction=None)
+
+        true_mask = tf.cast(tf.equal(label, tf.ones(shape=tf.shape(label))), tf.float32)
+        false_mask = 1 - true_mask
+
+        loss = true_mask * (1 - cosine_dist) + false_mask * (tf.maximum(0, cosine_dist - margin))
+
+        return tf.reduce_sum(loss)
+
+    def train_step(self, loss):
+        return tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(loss)
+
+    def accuracy(self, label, cosine_dist, threshold):
+
+        related_pred = tf.cast(tf.greater_equal(cosine_dist, threshold), dtype=tf.float32)
+        related_label = tf.cast(tf.equal(label, 1.0), dtype=tf.float32)
+
+        return tf.reduce_mean(tf.cast(tf.equal(related_pred, related_label), dtype=tf.float32))
