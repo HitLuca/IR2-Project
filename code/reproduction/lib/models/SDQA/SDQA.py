3fa8bcb7035b23417d985bb900da2c51424538ab
hengl <henglin.h@gmail.com>
2017-11-19 22:13:36 +0100

    added sparse2dense, fixed logit and inference, just need to fix the loss fn

--- code/reproduction/lib/models/SDQA/SDQA.py
+++ code/reproduction/lib/models/SDQA/SDQA.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 
 class SDQA:
-    def __init__(self, activation_fn, is_training, input_shape=48536,
-                 num_filter=32, num_hidden_fc=128, dropout=0.5, kernel_size=3,
+    def __init__(self, is_training, input_shape, activation_fn=tf.nn.relu,
+                 num_filter=8, num_hidden_fc=128, dropout=0.5, kernel_size=10,
                  learning_rate=0.01, batch_size=128):
 
         self.activation_fn = activation_fn
@@ -20,40 +20,47 @@ class SDQA:
         self.input1 = tf.placeholder(tf.float32, shape=[None, input_shape, 1])
         self.input2 = tf.placeholder(tf.float32, shape=[None, input_shape, 1])
 
-        with tf.variable_scope("SiameseNet", reuse=False):
+        with tf.variable_scope("SiameseNet", reuse=tf.AUTO_REUSE) as scope:
             self.logits1 = self._define_network(self.input1)
-        with tf.variable_scope("SiameseNet", reuse=True):
             self.logits2 = self._define_network(self.input2)
 
+        # with tf.variable_scope("SiameseNet", reuse=False):
+        #     self.logits1 = self._define_network(self.input1)
+        # with tf.variable_scope("SiameseNet", reuse=True):
+        #     self.logits2 = self._define_network(self.input2)
+
     def _define_network(self, features):
-        conv1 = tf.layers.conv1d(
-            inputs=features,
-            filters=self.num_filter,
-            kernel_size=self.kernel_size,
-            activation=self.activation_fn
-        )
+        with tf.variable_scope(None, default_name="Conv1") as scope:
+            conv1 = tf.layers.conv1d(
+                inputs=features,
+                filters=self.num_filter,
+                kernel_size=self.kernel_size,
+                activation=self.activation_fn
+            )
+            conv1 = tf.layers.max_pooling1d(inputs=conv1, pool_size=100, strides=1)
+            conv1 = tf.layers.dropout(conv1, rate=self.dropout, training=self.is_training)
 
-        pool1 = tf.layers.max_pooling1d(inputs=conv1, pool_size=2, strides=2)
-
-        dropout = tf.layers.dropout(pool1, rate=self.dropout, training=self.is_training)
-
-        logits = tf.layers.dense(inputs=dropout, units=self.num_hidden_fc)
+        with tf.variable_scope(None, default_name="Final_FC_Layers") as scope:
+            flattened = tf.contrib.layers.flatten(conv1)
+            logits = tf.layers.dense(inputs=flattened, units=self.num_hidden_fc)
 
         return logits
 
     def inference(self):
-        return tf.losses.cosine_distance(self.logits1, self.logits2, dim=0)
+        # TODO: Check if dim0 or dim1????
+        return tf.losses.cosine_distance(self.logits1, self.logits2, dim=1)
 
     def loss(self, label, margin=1):
 
-        cosine_dist = tf.losses.cosine_distance(self.logits1, self.logits2, dim=0)
+        # cosine_dist = tf.losses.cosine_distance(self.logits1, self.logits2, dim=0)
+        #
+        # true_mask = tf.cast(tf.equal(label, tf.ones(shape=tf.shape(label))), tf.float32)
+        # false_mask = 1 - true_mask
+        #
+        # loss = true_mask * (1 - cosine_dist) + false_mask * (tf.maximum(0, cosine_dist - margin))
 
-        true_mask = tf.cast(tf.equal(label, tf.ones(shape=tf.shape(label))), tf.float32)
-        false_mask = 1 - true_mask
-
-        loss = true_mask * (1 - cosine_dist) + false_mask * (tf.maximum(0, cosine_dist - margin))
-
-        return tf.reduce_sum(loss)
+        # return tf.reduce_sum(loss)
+        return tf.reduce_sum(label)
 
     def train_step(self, loss):
         return tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(loss)
--- code/reproduction/main.py
+++ code/reproduction/main.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+import time
 
 from code.reproduction.lib.data.quora_utils import QuoraDataset
 
@@ -6,72 +8,69 @@ from code.reproduction.lib.data.quora_utils import QuoraDataset
 from code.reproduction.lib.models.SDQA import SDQA
 from code.reproduction.lib.utils import cosine_sim_loss
 
-batch_size = 128
-learning_rate = 0.001
-activation_fn = tf.nn.relu
 restore = False
 
 # TODO: To be defined
 checkpoint_path = './ckpt/'
 checkpoint_prefix = 'ckpt_sdqa'
 
-##########################
-##########################
-'''
-NEED TO LOAD THE DATA
-'''
-##########################
-##########################
 
 dataset_folder = './../data/Quora'
 shuffle_dataset = True
-batch_size = 1
+batch_size = 20
 
 quora = QuoraDataset(dataset_folder)
 quora.init_dataset(shuffle_dataset, 'trigrams_sanitized')
 vocabulary_size = quora.get_vocabulary_size()
 
 # initialize the network
-nn = SDQA(activation_fn=activation_fn, is_training=True, input_shape=vocabulary_size)
+nn = SDQA(is_training=True, input_shape=vocabulary_size)
 
 input1 = nn.input1
 input2 = nn.input2
-labels = tf.placeholder(dtype=tf.float32, shape=[batch_size])
+labels = tf.placeholder(dtype=tf.float32, shape=[batch_size, 1])
 
 logits1 = nn.logits1
 logits2 = nn.logits2
 
 inference = nn.inference()
 loss = nn.loss(labels)
-train_step = nn.train_step(loss)
+# train_step = nn.train_step(loss)
 # accuracy = nn.accuracy() #TODO: check what to use here
 
-global_step = tf.Variable(0, name='global_step', trainable=False)
-saver = tf.train.Saver(max_to_keep=2)
+
+def sparse2dense(batch):
+    labels = np.array([sample['is_duplicate'] for sample in batch])
+    qid1 = np.array([sample['qid1'] for sample in batch])
+    qid2 = np.array([sample['qid2'] for sample in batch])
+    ids = np.array([sample['id'] for sample in batch])
+
+    # turn them to sparse representation
+    q1_vec = np.zeros(shape=[batch_size, vocabulary_size])
+    q2_vec = np.zeros(shape=[batch_size, vocabulary_size])
+
+    for i, sample in enumerate(batch):
+        q1_vec[i, sample['question1']] = 1
+    for i, sample in enumerate(batch):
+        q2_vec[i, sample['question2']] = 1
+
+    q1_vec = np.expand_dims(q1_vec, axis=2)
+    q2_vec = np.expand_dims(q2_vec, axis=2)
+    labels = np.expand_dims(labels, axis=1)
+
+    return ids, qid1, qid2, q1_vec, q2_vec, labels
+
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-
-    if restore:
-        # TODO: Not sure why tf.train.latest_checkpoint returns None???
-        saver.restore(sess, checkpoint_path)
-        last_global_step = global_step.eval()
-    else:
-        last_global_step = 0
-
-    print("Start training from:", last_global_step)
-
-    for i in range(100000):
-        print("--- iteration " + str(i + 1 + last_global_step) + " ---")
+    for i in range(1):
+        print("--- iteration " + str(i) + " ---")
         batch = quora.get_next_batch(batch_size)
-        # result = sess.run([], feed_dict={})
 
-        if i % 500 == 0:
-            save_path = saver.save(sess, checkpoint_prefix, global_step=global_step)
+        ids, qid1, qid2, q1_vec, q2_vec, y = sparse2dense(batch)
 
-    coord.request_stop()
-    coord.join(threads)
+        result = sess.run([logits1, logits2, inference, loss],
+                          feed_dict={input1: q1_vec, input2: q2_vec, labels: y})
+        print(result[0].shape)
