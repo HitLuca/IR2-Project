4c02c7a1f5b7350d7bcbd10fe7598f84cc059b23
HitLuca <luca.simonetto.94@gmail.com>
2017-11-22 17:21:19 +0100

    updated yahoo_utils. MAKE SURE TO RENAME THE PANDAS subject_content COLUMN into subject for the yahoo_df_subject+content.p dataset

--- code/reproduction/lib/data/yahoo_utils.py
+++ code/reproduction/lib/data/yahoo_utils.py
@@ -2,98 +2,144 @@ import pandas as pd
 import pickle
 import numpy as np
 import random
+import os
 
 pd.options.mode.chained_assignment = None
 
 
 class YahooDataset:
-    def __init__(self, dataset_folder):
+    class MissingVocabularyException(Exception):
+        pass
+
+    def __init__(self, dataset_folder, dataset_filename):
         self.dataset_folder = dataset_folder + '/'
-        self.dataset_filepath = self.dataset_folder + 'part_1.p'
-        self.vocabulary_filepath = self.dataset_folder + 'vocabulary.p'
+        self.dataset_filepath = self.dataset_folder + dataset_filename
+        self.vocabulary_filepath = self.dataset_folder + 'vocabulary_' + dataset_filename
         self.df = None
         self.vocabulary = None
         self.batch_index = 0
+        self.batch_indexes = None
+
+        print('loading_dataset...')
+        self._load_dataset()
+        print('loaded')
+
+        if self._vocabulary_exists():
+            print('loading vocabulary...')
+            self._load_vocabulary()
+            print('loaded')
+        else:
+            if not self._check_for_trigrams():
+                print('no trigrams in the dataset!')
+
+                print('generating trigrams...')
+                self._add_trigrams_to_dataset()
+                print('generated')
 
     def init_dataset(self, shuffle):
-        self.load_dataset()
-        self.load_dictionary()
-        self.df = list(self.df.T.to_dict().values())
+        self.batch_indexes = np.arange(len(self.df.index))
         if shuffle:
-            random.shuffle(self.df)
+            print('shuffling...')
+            np.random.shuffle(self.batch_indexes)
+            print('shuffled')
 
-    def load_dictionary(self):
-        self.vocabulary = self.load_from_binary(self.vocabulary_filepath)
+    def get_next_batch(self, batch_size):
+        self.batch_index += batch_size
+        selected_data = self.df.iloc[self.batch_indexes[self.batch_index - batch_size:self.batch_index], :]
+        return self._convert_pandas_to_list(selected_data)
 
     def get_vocabulary_size(self):
         if self.vocabulary is not None:
             return len(self.vocabulary)
         else:
-            return NotImplementedError
+            return self.MissingVocabularyException
 
-    def get_next_batch(self, batch_size):
-        self.batch_index += batch_size
-        return self.df[self.batch_index - batch_size:self.batch_index]
+    def _check_for_trigrams(self):
+        if 'tri_subject' in self.df.columns and 'tri_bestanswer' in self.df.columns:
+            return True
+        else:
+            return False
 
-    def load_dataset(self):
+    def _vocabulary_exists(self):
+        return os.path.exists(self.vocabulary_filepath)
+
+    def _load_dataset(self):
         self.df = pd.read_pickle(self.dataset_filepath)
 
-    def generate_trigrams(self):
-        self.load_dataset()
+    def _load_vocabulary(self):
+        self.vocabulary = np.array(self._load_from_binary(self.vocabulary_filepath))
 
+    def _shuffle_dataset(self):
+        random.shuffle(self.df)
+
+    def _generate_vocabulary(self, strings):
+        self.vocabulary = set()
+        for string in strings:
+            for i in range(len(string) - 2):
+                trigram = string[i:i + 3]
+                self.vocabulary.add(trigram)
+        self.vocabulary = list(self.vocabulary)
+        self._save_to_binary(self.vocabulary, self.vocabulary_filepath)
+        self.vocabulary = np.array(self.vocabulary)
+
+    def _add_trigrams_to_dataset(self):
         df = self.df
-        sel_1 = (df['content'] == '')
-        sel_2 = df['subject'] == df['content']
-        sel_3 = ~(df['bestanswer'] == '')
-        sel_4 = (df["bestanswer"].str.len() >= 3) & (df["subject"].str.len() >= 3)
-        sel = (sel_1 | sel_2) & sel_3 & sel_4
+
+        if 'subject_content' in df.columns:
+            df = df.rename(columns={'subject_content': 'subject'})
+            print('renamed column subject_content to subject')
+
+        if 'content' in df.columns:
+            sel_1 = (df['content'] == '')
+            sel_2 = df['subject'] == df['content']
+            sel_3 = ~(df['bestanswer'] == '')
+            sel_1 = (sel_1 | sel_2) & sel_3
+        else:
+            sel_1 = ~(df['bestanswer'] == '')
+
+        sel_2 = (df["bestanswer"].str.len() >= 3) & (df["subject"].str.len() >= 3)
+        sel = sel_1 & sel_2
         df_sel = df[sel]
 
         all_strings = list(df_sel['subject'].values) + list(df_sel['bestanswer'].values)
-        len(all_strings)
-        trigrams_vocabulary = set()
-        for string in all_strings:
-            for i in range(len(string) - 2):
-                trigram = string[i:i + 3]
-                trigrams_vocabulary.add(trigram)
-        trigrams_vocabulary = list(trigrams_vocabulary)
-        self._save_to_binary(trigrams_vocabulary, self.vocabulary_filepath)
-        trigrams_vocabulary = np.array(trigrams_vocabulary)
 
-        df_sel.loc["tri_subject"] = None
-        df_sel["tri_bestanswer"] = None
-        df_sel.head(1)
+        print('generating vocabulary...')
+        self._generate_vocabulary(all_strings)
+        print('generated')
 
-        sort_idx = np.array(trigrams_vocabulary).argsort()
+        df_sel["tri_subject"] = 'not_added_yet'
+        df_sel["tri_bestanswer"] = 'not_added_yet'
+
+        sort_idx = self.vocabulary.argsort()
 
         subject_list = list(df_sel['subject'].values)
         tri_subject_list = subject_list.copy()
         bestanswer_list = list(df_sel['bestanswer'].values)
         tri_bestanswer_list = bestanswer_list.copy()
 
+        print('generating trigrams for subject...')
         for i in range(len(subject_list)):
+            if i % 10000 == 0:
+                print(i, len(subject_list))
             subject = str(subject_list[i])
-            trigrams = self._extract_trigrams(subject, trigrams_vocabulary, sort_idx)
+            trigrams = self._extract_trigrams(subject, self.vocabulary, sort_idx)
             tri_subject_list[i] = trigrams
+        print(len(subject_list), len(subject_list))
 
+        print('generating trigrams for bestanswer...')
+        for i in range(len(bestanswer_list)):
+            if i % 10000 == 0:
+                print(i, len(bestanswer_list))
             bestanswer = str(bestanswer_list[i])
-            trigrams = self._extract_trigrams(bestanswer, trigrams_vocabulary, sort_idx)
+            trigrams = self._extract_trigrams(bestanswer, self.vocabulary, sort_idx)
             tri_bestanswer_list[i] = trigrams
+        print(len(bestanswer_list), len(bestanswer_list))
 
         df_sel['tri_subject'] = pd.Series(tri_subject_list)
         df_sel['tri_bestanswer'] = pd.Series(tri_bestanswer_list)
 
-        df_sel.to_pickle(self.dataset_filepath)
-
-    @staticmethod
-    def _save_to_binary(data, filepath):
-        with open(filepath, 'wb') as f:
-            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-
-    @staticmethod
-    def load_from_binary(filepath):
-        with open(filepath, 'rb') as f:
-            return pickle.load(f)
+        self._save_pandas(df_sel, self.dataset_filepath)
+        self.df = df_sel
 
     def _extract_trigrams(self, string, trigrams_vocabulary, sort_idx):
         trigrams = np.unique([string[j:j + 3] for j in range(len(string) - 2)])
@@ -103,12 +149,37 @@ class YahooDataset:
 
         return np.unique(sort_idx[np.searchsorted(trigrams_vocabulary, trigrams, sorter=sort_idx)])
 
+    @staticmethod
+    def _convert_pandas_to_list(data):
+        return list(data.T.to_dict().values())
+
+    @staticmethod
+    def _save_to_binary(data, filepath):
+        with open(filepath, 'wb') as f:
+            pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def _load_from_binary(filepath):
+        with open(filepath, 'rb') as f:
+            return pickle.load(f)
+
+    @staticmethod
+    def _save_pandas(data, filepath):
+        data.to_pickle(filepath)
+
+
+dataset_filenames = ['part_1.p',
+                     'yahoo_df_subject+content.p',
+                     'yahoo_df_subject.p',
+                     'yahoo_df_subject_no_content.p']
 
 dataset_folder = './../../../data/Yahoo'
-shuffle_dataset = True
+dataset_filename = dataset_filenames[0]
+
+shuffle_dataset = False
 batch_size = 1
 
-yahoo = YahooDataset(dataset_folder)
+yahoo = YahooDataset(dataset_folder, dataset_filename)
 yahoo.init_dataset(shuffle_dataset)
-print(yahoo.get_vocabulary_size())
+print('vocabulary size:', yahoo.get_vocabulary_size())
 print(yahoo.get_next_batch(batch_size))
