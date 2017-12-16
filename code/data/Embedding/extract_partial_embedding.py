b75ed002dcdcdd303a30a3cde8d7b6be02d9a076
hengl <henglin.h@gmail.com>
2017-12-16 13:01:22 +0100

    modified the script to extract embedding matrix

--- code/data/Embedding/extract_partial_embedding.py
+++ code/data/Embedding/extract_partial_embedding.py
@@ -20,17 +20,41 @@ import pickle
 vocabulary = np.load("/media/henglin/data/ir2_tmp/embedding_vocabulary.npy")
 embedding_matrix = np.load("/media/henglin/data/ir2_tmp/embedding_matrix.npy")
 
+# Define the path to all the vocabulary files
+vocab_paths = {"train": "../Yahoo/vocab_train_reduced.p",
+               "val": "../Yahoo/vocab_val_reduced.p",
+               "test": ""}
+
 # path of the vocabulary of interest
-test_vocab_path = "../reproduction/lib/data/testset_vocab.txt"
+test_vocab_path = "../Yahoo/testset_vocab.txt"
 
 # load the vocab of interest into numpy array
 with open(test_vocab_path, 'r') as f:
-    yahoo_vocab = np.array([word.rstrip() for word in f])
+    # test_vocab = np.array([word.rstrip() for word in f])
+    test_vocab = [word.rstrip() for word in f]
 
-print("Vocabulary size: {}".format(len(vocabulary)))
-print("Yahoo Vocabulary size: {}".format(len(yahoo_vocab)))
+all_vocab = list()
 
-mask = np.in1d(vocabulary, yahoo_vocab)     # find the overlap of vocab
+# load vocabulary from each partition
+train_vocab = pickle.load(open(vocab_paths["train"], "rb"))
+val_vocab = pickle.load(open(vocab_paths["val"], "rb"))
+
+print("Training vocab:   {}".format(len(train_vocab)))
+print("Validation vocab: {}".format(len(val_vocab)))
+print("Testing vocab:    {}".format(len(test_vocab)))
+
+# Combine the vocabulary for training, validation, and testing
+all_vocab += train_vocab
+all_vocab += val_vocab
+all_vocab += test_vocab
+
+# remove redundancies
+all_vocab = np.unique(np.array(all_vocab))
+
+print("Embedding Vocabulary size: {}".format(len(vocabulary)))
+print("Total Vocabulary size:     {}".format(all_vocab.shape[0]))
+
+mask = np.in1d(vocabulary, all_vocab)     # find the overlap of vocab
 idx = np.where(mask == True)                # find the index of overlapped vocab
 
 partial_embedding_matrix = embedding_matrix[idx[0], :]  # select the part from original embedding matrix
@@ -40,7 +64,7 @@ print(partial_embedding_matrix.shape)
 r2 = np.zeros(shape=[2, partial_embedding_matrix.shape[1]])
 partial_embedding_matrix = np.concatenate([partial_embedding_matrix, r2], axis=0)
 
-np.save("testset_partial_embedding_matrix", partial_embedding_matrix)
+np.save("partial_embedding_matrix", partial_embedding_matrix)
 
 # select the overlapped vocabulary
 selected_vocab = vocabulary[idx[0]]
